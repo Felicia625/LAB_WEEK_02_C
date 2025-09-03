a442e2656c97c2d5eca97b9f34e15ccb53fb239e
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2025-09-03 20:46:03 +0700

    Commit No.4 - Standard activity and single top activity

--- app/src/main/java/com/example/lab_week_02_c/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Button
+import android.content.Intent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DEBUG = "DEBUG"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d(DEBUG, "onCreate")
+        val buttonClickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.button_standard -> startActivity(
+                    Intent(this,
+                        StandardActivity::class.java)
+                )
+                R.id.button_single_top -> startActivity(
+                    Intent(this,
+                        SingleTopActivity::class.java)
+                )
+            }
+        }
+
+        findViewById<Button>(R.id.button_standard).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button_single_top).setOnClickListener(buttonClickListener)
     }
 }
